16879399747a60d36444218c8275e03ba292f159
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-07 17:42:03 -0300

    configuração inicial nos arquivos

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Strella</title>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <p>Olá, mundo!</p>
+</body>
+</html>
