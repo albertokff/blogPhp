005736af7ef7d21adca2eac35bc76a2e13451ec8
Diogo  Alberto <106744072+albertokff@users.noreply.github.com>
2023-08-08 13:17:29 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -1,6 +1,7 @@
 <?php
     include_once("templates/header.php");
 ?>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
     <h1>Página de contato!</h1>
 <?php
     include_once("templates/footer.php");
