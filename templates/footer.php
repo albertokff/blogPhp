bf192f7f7330335d9cef1535a5072c88684c95be
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-08 08:46:25 -0300

    CSS do cabeçalho e do rodapé

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
 <footer>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/footer.css">
     <p>Soluct &copy; 2023</p>
 </footer>
 </body>
--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Strella</title>
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/navbar.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
@@ -19,10 +20,10 @@
         </a>
         <nav>
             <ul id="navbar">
-                <li><a href="<?= $BASE_URL ?>">Home</a></li>
-                <li><a href="#">Categorias</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="<?= $BASE_URL ?>contato.php">Contato</a></li>
+                <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Categorias</a></li>
+                <li><a href="#" class="nav-link">Sobre</a></li>
+                <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
             </ul>
         </nav>
     </header>
